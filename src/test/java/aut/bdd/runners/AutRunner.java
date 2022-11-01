8d2cf21b4f52a52b869cbfe3c5d7a950867e75fd
faheloDev <fahelo.dev@gmail.com>
2022-11-01 12:29:54 -0300

    Semana 6 : Framework - Cucumber - Init

---
+++ src/test/java/aut/bdd/runners/AutRunner.java
@@ -0,0 +1,15 @@
+package aut.bdd.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import lombok.extern.apachecommons.CommonsLog;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"},
+        glue = {"aut.bdd.steps","aut.bdd.runners"},
+        tags = {""},
+        features = {"src/test/java/aut/bdd/features"})
+@CommonsLog
+public class AutRunner {
+}
